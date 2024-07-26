a3800d48dc79d4d837704bd1c6da5c7c7d58dfe0
Anupama462 <anupamapotluri05@gmail.com>
2024-07-26 10:51:36 +0530

    uploadfile

--- src/test/java/practice/fileupload.java
+++ src/test/java/practice/fileupload.java
@@ -8,16 +8,20 @@ import org.testng.annotations.BeforeTest;
 
 public class fileupload {
 	WebDriver driver;
+	
+	
   @Test
   public void uploadfile() throws InterruptedException {
-	  Thread.sleep(2000);
+	  Thread.sleep(3000);
 	  driver.get("http://baalabharathi.com/upload-story/");
 	  Thread.sleep(2000);
 	  driver.findElement(By.xpath("//*[@id=\"field-SUtF8PwfCXGXx46\"]")).sendKeys("techlearn.in");
-	  
-	  
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//*[@id=\"field-8lqEle2zeOhMu0a\"]")).sendKeys("F:\\ANUPAMA\\ANU.jpg");
 	  
   }
+ 
+  
   @BeforeTest
   public void beforeTest() {
 	  driver = new ChromeDriver();
