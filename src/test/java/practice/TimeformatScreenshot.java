429f5b6191baa54915740d8e78cfc21aeaa49fc8
Anupama462 <anupamapotluri05@gmail.com>
2024-07-26 21:35:00 +0530

    TimeScreenshot

---
+++ src/test/java/practice/TimeformatScreenshot.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class TimeformatScreenshot {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
