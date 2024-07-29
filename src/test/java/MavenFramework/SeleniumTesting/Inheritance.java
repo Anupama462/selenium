6da68a699f1812ff00a5c0db62e728589c463ec6
Anupama462 <anupamapotluri05@gmail.com>
2024-07-29 19:11:13 +0530

    Inheritance

---
+++ src/test/java/MavenFramework/SeleniumTesting/Inheritance.java
@@ -0,0 +1,22 @@
+package MavenFramework.SeleniumTesting;
+
+public class Inheritance {
+	
+	public void start()
+	{
+		System.out.println("Started");
+	}
+
+	public void stop()
+	{
+		System.out.println("Stopped");
+	}
+
+}
+
+
+	  
+	  
+	  
+	  
+  
