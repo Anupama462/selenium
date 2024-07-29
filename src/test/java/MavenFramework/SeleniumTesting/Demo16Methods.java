3addda85e08998ad01254ab412d8b0ef7935a342
Anupama462 <anupamapotluri05@gmail.com>
2024-07-29 10:32:06 +0530

    Methods Demo

---
+++ src/test/java/MavenFramework/SeleniumTesting/Demo16Methods.java
@@ -0,0 +1,24 @@
+package MavenFramework.SeleniumTesting;
+
+public class Demo16Methods {
+
+	public static void main(String[] args) {
+	
+		Demo16Methods md = new Demo16Methods();
+		
+	
+	}
+
+	public void doLogin()
+	{//start
+		
+		System.out.println("Login Successful");
+		
+	}//end
+	
+	public void doLogout()
+	{
+		doLogout();
+		System.out.println("Logout successful");
+	}
+}
