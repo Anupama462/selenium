321f696316c219364693cacb607b0d9116d22462
Anupama462 <anupamapotluri05@gmail.com>
2024-07-29 19:33:00 +0530

    Car Inheritance

---
+++ src/test/java/MavenFramework/SeleniumTesting/Car.java
@@ -0,0 +1,14 @@
+package MavenFramework.SeleniumTesting;
+
+public class Car extends Inheritance {
+
+	public static void main(String[] args) {
+		
+		Car ca = new Car();
+		ca.start();
+		ca.stop();
+		
+
+	}
+
+}
