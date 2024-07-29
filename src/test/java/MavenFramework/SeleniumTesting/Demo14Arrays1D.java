d3a1bcd79452c49f8a0542809eba61f5736f93e1
Anupama462 <anupamapotluri05@gmail.com>
2024-07-29 09:43:09 +0530

    Arrays 1 dimentional

---
+++ src/test/java/MavenFramework/SeleniumTesting/Demo14Arrays1D.java
@@ -0,0 +1,34 @@
+package MavenFramework.SeleniumTesting;
+
+public class Demo14Arrays1D {
+
+	public static void main(String[] args) {
+		
+		int myInt = 2;
+		int[] myIntArray = new int[10];
+		myIntArray[0] = 10;
+		myIntArray[1] = 20;
+		myIntArray[2] = 30;
+		myIntArray[3] = 40;
+		System.out.println(myIntArray[3]);
+		System.out.println(myIntArray.length);
+		
+		int[] myIntArray1 = {10,20,30,40};
+		int myIntArray2[] = {10,20,30,40};
+		System.out.println(myIntArray1[1]);
+		System.out.println(myIntArray1.length);
+		
+		String[] myStringArray = new String[5];
+		byte[] anArrayOfBytes;
+		short[] anArrayOfShorts;
+		long[] anArrayOfLongs;
+		float[] anArrayOfFloats;
+		double[] anArrayOfDoubles;
+		boolean[] anArrayOfBooleans;
+		char[] anArrayOfChars;
+		String[] anArrayOfStrings;
+		
+		
+	}
+
+}
