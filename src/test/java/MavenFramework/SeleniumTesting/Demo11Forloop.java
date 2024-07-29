a0a5ed3042e69b6402725739069d703ed55ab78c
Anupama462 <anupamapotluri05@gmail.com>
2024-07-29 07:01:16 +0530

    For loop commited

---
+++ src/test/java/MavenFramework/SeleniumTesting/Demo11Forloop.java
@@ -0,0 +1,23 @@
+package MavenFramework.SeleniumTesting;
+
+public class Demo11Forloop {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		for(;i<=10;)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	//	While loop - dont know the number of iterations
+	//	do while - dont know the number of iterations - want to execute the code at least once irrespective of
+	//	what ever the condition is
+		
+	//	For loop - how many iterations you want to run
+
+	}
+
+}
