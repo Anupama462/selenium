76b3b4709224a4973ad73fdae8e43935b5ea5c3b
Anupama462 <anupamapotluri05@gmail.com>
2024-07-30 08:23:26 +0530

    Final Keyword

---
+++ src/test/java/finalKeyword/Child.java
@@ -0,0 +1,20 @@
+package finalKeyword;
+
+public class Child extends Parent{
+	
+	public final int i = 10;
+
+	public static void main(String[] args) {
+		
+		Child c = new Child();
+		System.out.println(c.i);
+		//c.i=20;
+		System.out.println(c.i);
+
+	}
+	
+	/*public void setData() {
+		System.out.println(i); 
+	}                                */
+
+}
