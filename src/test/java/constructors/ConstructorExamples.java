b925b47a6424463e4fc3ca8cef86a42a3ce0f433
Anupama462 <anupamapotluri05@gmail.com>
2024-07-30 07:23:03 +0530

    Constructors

---
+++ src/test/java/constructors/ConstructorExamples.java
@@ -0,0 +1,34 @@
+package constructors;
+
+public class ConstructorExamples {
+	//No return type
+	//Same name as the class name
+	
+	public ConstructorExamples() {
+		System.out.println("no argument constructor");
+		
+	}
+
+	public ConstructorExamples(int i) {
+		System.out.println("Integer constructor");
+		
+	}
+	
+	public ConstructorExamples(int i, int y) {
+		System.out.println("2 integer constructor");
+		
+	}
+	
+	public ConstructorExamples(String s) {
+		System.out.println("String constructor");
+		
+	}
+	public static void main(String[] args) {
+		
+		ConstructorExamples ce = new ConstructorExamples(5,6);
+		
+		
+
+	}
+
+}
